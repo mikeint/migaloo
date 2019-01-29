845857fa3a1a495697d65ddbd93f2b3487fd2621
mikeint <michael.sansone@hotmail.com>
2019-01-28 21:32:10 -0500

    removing errors

--- client/src/layout/Landing/Landing.js
+++ client/src/layout/Landing/Landing.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'; 
-import { Route, NavLink, BrowserRouter } from 'react-router-dom';
+import { /* Route ,*/ NavLink, BrowserRouter } from 'react-router-dom';
 
 import './Landing.css'; 
 
