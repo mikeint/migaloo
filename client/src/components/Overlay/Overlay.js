6d7f89fdb084669f85dbf7a4f97c11ab5827d5ca
Sansone <Michael.sansone@hotmail.com>
2019-02-14 20:49:13 -0500

    adding my client to postgres branch

--- client/src/components/Overlay/Overlay.js
+++ client/src/components/Overlay/Overlay.js
@@ -3,33 +3,37 @@ import "./Overlay.css";
 
 class Overlay extends Component {
 
-    componentDidMount = () => {
-        document.querySelector(".back").addEventListener("click", this.toggleClassMenu, false);
-        document.querySelector(this.props.class).addEventListener("click", this.toggleClassMenu, false);
+    componentDidMount = () => { 
+        console.log("overlay loaded");
+        console.log(document.querySelector(".menu"))
+        setTimeout(() => {
+            this.toggleClassMenu();
+        }, 100); 
     }
 
     toggleClassMenu() {
-        document.querySelector(".menu").classList.add("menu--animatable");
-        if(!document.querySelector(".menu").classList.contains("menu--visible")) {
-            document.querySelector(".menu").classList.add("menu--visible");
+        console.log(document.querySelector(".menu"))
+        document.querySelector(".menu").classList.add("menu--animatable");	
+        if(!document.querySelector(".menu").classList.contains("menu--visible")) {		
+            document.querySelector(".menu").classList.add("menu--visible"); 
         } else {
-            document.querySelector(".menu").classList.remove('menu--visible');		
-        }
-    }
+            document.querySelector(".menu").classList.remove('menu--visible');
+            setTimeout(() => { 
+                this.props.callOverlay(); 
+            }, 150);
+        } 
+    } 
 
     render() {
+ 
+        const { config } = this.props; 
 
         return (
             <React.Fragment>
                 <div className="menu">
-                    <div className="app-menu">
-                        <div className="back">x</div>
-                        <div className="jobTitle">Front end web developer</div>
-                        <div className="jobDescTitle">Job Description</div>
-                        <div className="jobDescription">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</div>
-                        <div className="jobDescApplicantsContainer">
-                            <div className="jobApplicantTitle">This job has received 3 aplicants so far</div>
-                        </div>
+                    <div className={"app-menu "  + config.direction}>
+                        <div className={"back " + config.backButtonLocation} onClick={this.toggleClassMenu.bind(this)}>x</div> 
+                        <div id="overlayHtml" dangerouslySetInnerHTML={{ __html: this.props.html }} />
                     </div>
                 </div>
             </React.Fragment>
