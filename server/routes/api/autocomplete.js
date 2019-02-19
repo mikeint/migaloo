caed9b4829e670b9842e582aeb425fccf642124b
Michael Marcucci <onlycutch@gmail.com>
2019-02-18 23:34:57 -0500

    Added lower case to the tags, and ability to add tags

--- server/routes/api/autocomplete.js
+++ server/routes/api/autocomplete.js
@@ -15,9 +15,9 @@ const postgresdb = require('../../config/db').postgresdb;
 router.get('/experience/:find', passport.authentication, (req, res) => {
     postgresdb.any('SELECT experience_type_name, experience_type_id \
             FROM experience_type \
-            WHERE experience_type_name LIKE $1 \
+            WHERE lower(experience_type_name) LIKE $1 \
             ORDER BY experience_type_name ASC \
-            LIMIT 10', "%"+req.params.find+"%").then(data => {
+            LIMIT 10', "%"+req.params.find.toLowerCase()+"%").then(data => {
         res.json(data);
     })
     .catch(err => {
@@ -48,9 +48,9 @@ router.get('/experience', passport.authentication, (req, res) => {
 router.get('/tag/:find', passport.authentication, (req, res) => {
     postgresdb.any('SELECT tag_name, tag_id \
             FROM tags \
-            WHERE tag_name LIKE $1 \
+            WHERE lower(tag_name) LIKE $1 \
             ORDER BY tag_name ASC \
-            LIMIT 10', "%"+req.params.find+"%").then(data => {
+            LIMIT 10', "%"+req.params.find.toLowerCase()+"%").then(data => {
         res.json(data);
     })
     .catch(err => {
@@ -71,6 +71,28 @@ router.get('/tag', passport.authentication, (req, res) => {
 });
 
 /**
+ * Get tag by autocomplete
+ * @route GET api/autocomplete/tag
+ * @group autocomplete - Autocomplete
+ * @returns {object} 200 - A list of maps containing autocompletes
+ * @returns {Error}  default - Unexpected error
+ * @access Private
+ */
+router.post('/addTag/:tag', passport.authentication, (req, res) => {
+    // TODO: BIG NOTE, XSS potential ensure we are using pre tag on front end, and cleaning this out periodically
+    var tag = req.params.tag
+    postgresdb.one('INSERT INTO tags (tag_name) VALUES ($1) RETURNING tag_id', tag)
+    .then(data => {
+        console.log({tag_id: data.tag_id, tag_name:tag})
+        res.json({tag_id: data.tag_id, tag_name:tag});
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(400).json(err)
+    });
+});
+
+/**
  * Get salary by autocomplete
  * @route GET api/autocomplete/salary
  * @group autocomplete - Autocomplete
@@ -81,9 +103,9 @@ router.get('/tag', passport.authentication, (req, res) => {
 router.get('/salary/:find', passport.authentication, (req, res) => {
     postgresdb.any('SELECT salary_type_name, salary_type_id \
             FROM salary_type \
-            WHERE salary_type_name LIKE $1 \
+            WHERE lower(salary_type_name) LIKE $1 \
             ORDER BY salary_type_name ASC \
-            LIMIT 10', "%"+req.params.find+"%").then(data => {
+            LIMIT 10', "%"+req.params.find.toLowerCase()+"%").then(data => {
         res.json(data);
     })
     .catch(err => {
