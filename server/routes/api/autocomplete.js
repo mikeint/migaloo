8cac3edf150fe0f2c33ddb676413b9a71ea2bb57
Michael Marcucci <onlycutch@gmail.com>
2019-02-18 22:44:45 -0500

    Added Extra autocomplete functions for a default call

--- server/routes/api/autocomplete.js
+++ server/routes/api/autocomplete.js
@@ -25,6 +25,17 @@ router.get('/experience/:find', passport.authentication, (req, res) => {
         res.status(400).json(err)
     });
 });
+router.get('/experience', passport.authentication, (req, res) => {
+    postgresdb.any('SELECT experience_type_name, experience_type_id \
+            FROM experience_type \
+            ORDER BY experience_type_name ASC').then(data => {
+        res.json(data);
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(400).json(err)
+    });
+});
 
 /**
  * Get tag by autocomplete
@@ -47,6 +58,17 @@ router.get('/tag/:find', passport.authentication, (req, res) => {
         res.status(400).json(err)
     });
 });
+router.get('/tag', passport.authentication, (req, res) => {
+    postgresdb.any('SELECT tag_name, tag_id \
+            FROM tags \
+            ORDER BY tag_name ASC').then(data => {
+        res.json(data);
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(400).json(err)
+    });
+});
 
 /**
  * Get salary by autocomplete
@@ -62,6 +84,17 @@ router.get('/salary/:find', passport.authentication, (req, res) => {
             WHERE salary_type_name LIKE $1 \
             ORDER BY salary_type_name ASC \
             LIMIT 10', "%"+req.params.find+"%").then(data => {
+        res.json(data);
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(400).json(err)
+    });
+});
+router.get('/salary', passport.authentication, (req, res) => {
+    postgresdb.any('SELECT salary_type_name, salary_type_id \
+            FROM salary_type \
+            ORDER BY salary_type_name ASC').then(data => {
         res.json(data);
     })
     .catch(err => {
