2b4705e98ec068bcc73e916f46321d9dc0cd08b0
Patrick Robertshaw <patrick_robertshaw@hotmail.com>
2019-02-19 05:53:21 -0500

    reformmating docker setup to be easier

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,10 @@
 var pgp = require('pg-promise')();
 const cn = {
-    host: process.env.DB_HOST | 'localhost',
-    port: process.env.DB_PORT | 5432,
-    database: process.env.DB_DATABASE | 'hireranked_prod',
-    user: process.env.DB_USERNAME | 'postgres',
-    password: process.env.DB_PASSWORD | 'postgres'
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
+    database: process.env.DB_DATABASE || 'hireranked_prod',
+    user: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres'
 };
 var db = pgp(cn);
 module.exports = {
