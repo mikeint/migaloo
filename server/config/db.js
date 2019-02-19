49cb2038067b635d72f5c1f3a7eebe4a8e644989
Patrick Robertshaw <patrick_robertshaw@hotmail.com>
2019-02-19 04:49:08 -0500

    dockerization

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,10 @@
 var pgp = require('pg-promise')();
 const cn = {
-    host: 'localhost',
-    port: 5432,
-    database: 'hireranked_prod',
-    user: 'postgres',
-    password: 'postgres'
+    host: process.env.DB_HOST | 'localhost',
+    port: process.env.DB_PORT | 5432,
+    database: process.env.DB_DATABASE | 'hireranked_prod',
+    user: process.env.DB_USERNAME | 'postgres',
+    password: process.env.DB_PASSWORD | 'postgres'
 };
 var db = pgp(cn);
 module.exports = {
