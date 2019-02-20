2517b1d60026ea90664c2ae61d285b3a33a6199a
Sansone <Michael.sansone@hotmail.com>
2019-02-19 21:34:49 -0500

    fix landing slider

--- client/src/layout/LandingSection2/LandingSection2.js
+++ client/src/layout/LandingSection2/LandingSection2.js
@@ -47,48 +47,42 @@ class LandingSection2 extends Component {
         return (
         <div className="landingSection2">  
             <Slider {...settings}>
-				<ScrollAnimation animateIn='bounceInRight' duration={1}>
+				<ScrollAnimation animateIn='bounceInLeft' duration={2} >
                     <div>
                         <img src={carouselImg1} alt="" />
                         <h1 className="mc_4_text">Real Recruitment Platform</h1>
                         <p>The first and only platform connecting employers directly to third-party recruiters.</p>
                     </div>
                 </ScrollAnimation>
-				<ScrollAnimation animateIn='bounceInRight' duration={2}>
+				<ScrollAnimation animateIn='flipInY' duration={2}>
                     <div>
                         <img src={carouselImg2} alt="" />
                         <h1 className="mc_4_text">Top Quality Candidates</h1>
                         <p>Recruiters have the opportunity to showcase their best candidates, and employers get a ranked list of top talent to choose from.</p>
                     </div>
                 </ScrollAnimation>
-				<ScrollAnimation animateIn='bounceInRight' duration={3}>
+				<ScrollAnimation animateIn='bounceInRight' duration={2}>
                     <div>
                         <img src={carouselImg3} alt="" />
                         <h1 className="mc_4_text">Effiecient</h1>
                         <p>Employers will only view qualified candidates. Recruiters get the right information upfront so they can assess their candidate pool to see if they have a match.  </p>
                     </div>
-                </ScrollAnimation>
-                <ScrollAnimation animateIn='bounceInRight' duration={4}>
-                    <div>
-                        <img src={carouselImg4} alt="" />
-                        <h1 className="mc_4_text">Credit-Based System</h1>
-                        <p>Recruiters can rank ahead of their competition based on the confidence in their ability to fill the opening.  Employers get an organized list of qualified candidates.</p>
-                    </div>
                 </ScrollAnimation> 
-				<ScrollAnimation animateIn='bounceInRight' duration={4}>
-                    <div>
-                        <img src={carouselImg5} alt="" />
-                        <h1 className="mc_4_text">Transparency</h1>
-                        <p>Each submission has recruiters' terms and conditions included allowing employers to make informed decisions on which recruiter(s) they choose to engage. </p>
-                    </div>
-                </ScrollAnimation>
-				<ScrollAnimation animateIn='bounceInRight' duration={5}>
-                    <div>
-                        <img src={carouselImg6} alt="" />
-                        <h1 className="mc_4_text">Confidentiality</h1>
-                        <p>Both employer and candidate details are confidential. Only when successful matches have been made are details divulged.</p>
-                    </div>
-                </ScrollAnimation> 
+                <div>
+                    <img src={carouselImg4} alt="" />
+                    <h1 className="mc_4_text">Credit-Based System</h1>
+                    <p>Recruiters can rank ahead of their competition based on the confidence in their ability to fill the opening.  Employers get an organized list of qualified candidates.</p>
+                </div> 
+                <div>
+                    <img src={carouselImg5} alt="" />
+                    <h1 className="mc_4_text">Transparency</h1>
+                    <p>Each submission has recruiters' terms and conditions included allowing employers to make informed decisions on which recruiter(s) they choose to engage. </p>
+                </div> 
+                <div>
+                    <img src={carouselImg6} alt="" />
+                    <h1 className="mc_4_text">Confidentiality</h1>
+                    <p>Both employer and candidate details are confidential. Only when successful matches have been made are details divulged.</p>
+                </div> 
             </Slider>
           </div>
         );
