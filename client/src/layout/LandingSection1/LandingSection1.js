14ba50ecadc3a4527889833b692aed0c7f648b20
Sansone <Michael.sansone@hotmail.com>
2019-02-21 19:15:09 -0500

    layout pages

--- client/src/layout/LandingSection1/LandingSection1.js
+++ client/src/layout/LandingSection1/LandingSection1.js
@@ -10,21 +10,27 @@ class LandingSection1 extends Component {
 
         return (
             <div className="landingSection1">  
-                <div className="full">
-                    <div className="half whiteBG centerText maxTextW">
-                        <div className="csImg"><img src={currStateImg} alt="" /></div>
-                        <h2>The Current State</h2>
-                        <p>The skilled job market is candidate driven and growing. Employers now more than ever need talented third-party recruiters for hard-to-fill&nbsp;jobs.</p>
+                <div className="module_wrapper">
+                    <div className="animated_half centerText">
+                        <div className="module_content">
+                            <div className="csImg"><img src={currStateImg} alt="" /></div>
+                            <h2>The Current State</h2>
+                            <p className="maxTextW">The skilled job market is candidate driven and growing. Employers now more than ever need talented third-party recruiters for hard-to-fill&nbsp;jobs.</p>
+                        </div>
                     </div>
-                    <div className="half whiteBG centerText maxTextW">
-                        <div className="csImg"><img src={problemImg} alt="" /></div>
-                        <h2>The Problem</h2>
-                        <p>Employers are overwhelmed by the growing number of hungry recruiters competing for their business. They typically end up engaging familiar recruiters, but should they feel confident they're truly getting the best candidates on the&nbsp;market?</p>
+                    <div className="animated_half centerText">
+                        <div className="module_content">
+                            <div className="csImg"><img src={problemImg} alt="" /></div>
+                            <h2>The Problem</h2>
+                            <p className="maxTextW">Employers are overwhelmed by the growing number of hungry recruiters competing for their business. They typically end up engaging familiar recruiters, but should they feel confident they're truly getting the best candidates on the&nbsp;market?</p>
+                        </div>
                     </div>
                 </div> 
             </div>
         );
     }
 }
+ 
+
 
 export default LandingSection1;
--- client/src/layout/LandingSection3/LandingSection3.js
+++ client/src/layout/LandingSection3/LandingSection3.js
@@ -1,12 +1,7 @@
 import React, { Component } from "react";  
 import './LandingSection3.css';
 import { NavLink } from 'react-router-dom';
-import ScrollAnimation from 'react-animate-on-scroll'; 
- 
-import carouselImg3 from '../../files/images/landingPage/carousel-3.png'; 
-import carouselImg4 from '../../files/images/landingPage/carousel-4.png'; 
-import carouselImg5 from '../../files/images/landingPage/carousel-5.png'; 
-import carouselImg6 from '../../files/images/landingPage/carousel-6.png';   
+import ScrollAnimation from 'react-animate-on-scroll';  
 
 class LandingSection3 extends Component {
   
@@ -14,78 +9,139 @@ class LandingSection3 extends Component {
 
         return (
             <div className="landingSection3">  
+
+
+
+            
+                {/* FOR DESKTOP - stacking properly */ }
+                <div className="showOnDesktop">
                     <div className="module_wrapper mc_4">
-                        <ScrollAnimation animateIn='bounceInLeft' duration={2} animateOnce={true}> 
-                            <div className=""> 
-                                <img src={carouselImg6} alt="" /> 
-                            </div>
+                        <ScrollAnimation animateIn='bounceInLeft' duration={1} animateOnce={true}> 
+                            <div className="module_img module_img1"></div>
                         </ScrollAnimation>
-                        <ScrollAnimation animateIn='bounceInRight' duration={2} animateOnce={true}> 
-                            <div className="moduleText">
-                                <div className="module_content">
-                                    <h1>Sign up</h1>
-                                    <p>Employers sign up to post jobs exclusively to third-party recruiters. Recruiters sign up to gain access to jobs posted by employers seeking third-party recruitment services.</p>
-                                </div>
+                        <ScrollAnimation animateIn='bounceInRight' duration={1} animateOnce={true}>  
+                            <div className="module_content">
+                                <h1>Sign up</h1>
+                                <p>Employers sign up to post jobs exclusively to third-party recruiters. Recruiters sign up to gain access to jobs posted by employers seeking third-party recruitment services.</p>
                             </div>
                         </ScrollAnimation>
                     </div>
-                 
                     <div className="module_wrapper">
-                        <ScrollAnimation animateIn='bounceInLeft' duration={3} animateOnce={true}>
-                            <div className="moduleText">
-                                <div className="module_content">
-                                    <h1>Submit Profile</h1>
-                                    <p>Recruiters submit their top candidate profiles along with terms and conditions to relevant job postings</p>
-                                </div>
-                            </div>
+                        <ScrollAnimation animateIn='bounceInLeft' duration={1} animateOnce={true}> 
+                            <div className="module_content">
+                                <h1>Submit Profile</h1>
+                                <p>Recruiters submit their top candidate profiles along with terms and conditions to relevant job postings</p>
+                            </div> 
                         </ScrollAnimation>
-                        <ScrollAnimation animateIn='bounceInRight' duration={3} animateOnce={true}>
-                            <div className=""> 
-                                <img src={carouselImg5} alt="" /> 
-                            </div>
-                        </ScrollAnimation>
-                    </div>  
-                    <div className="module_wrapper mc_4">
-                        <ScrollAnimation animateIn='bounceInLeft' duration={3} animateOnce={true}> 
-                            <div className="">    
-                                <img src={carouselImg3} alt="" /> 
-                            </div>
-                        </ScrollAnimation>
-                        <ScrollAnimation animateIn='bounceInRight' duration={3} animateOnce={true}> 
-                            <div className="moduleText">
-                                <div className="module_content">
-                                    <h1>Review List</h1>
-                                    <p>Employer reviews the credit-based, organized list and picks the candidate(s) that they wish to move forward with based on their profile and the recruiters terms and conditions</p>
-                                </div>
-                            </div>
-                        </ScrollAnimation>
-                    </div> 
-                    <div className="module_wrapper">
-                        <ScrollAnimation animateIn='bounceInLeft' duration={3} animateOnce={true}> 
-                            <div className="moduleText"> 
-                                <div className="module_content">
-                                    <h1>Engage</h1>
-                                    <p>Employer can reach out to the recruiter(s) they’ve chosen to engage based on their candidate profile submission and terms and conditions via our messaging app, email, or phone</p>
-                                </div>
-                            </div>
-                        </ScrollAnimation>
-                        <ScrollAnimation animateIn='bounceInRight' duration={3} animateOnce={true}> 
-                            <div className=""> 
-                                <img src={carouselImg4} alt="" /> 
-                            </div>
+                        <ScrollAnimation animateIn='bounceInRight' duration={1} animateOnce={true}> 
+                            <div className="module_img module_img2"></div> 
                         </ScrollAnimation>
                     </div>
-                <div className="module_wrapper greyBG">
-                    <div className="module_half moduleText">
-                        <div className="module_content"> 
-                            <h1>Ready to find out more?</h1>
-                            <p>We are currently working towards finishing our plat- Sign Up form. Please sign up below to get word about when our FREE version launches. </p>
+                    <div className="module_wrapper mc_4">
+                        <ScrollAnimation animateIn='bounceInLeft' duration={1} animateOnce={true}> 
+                            <div className="module_img module_img3"></div>
+                        </ScrollAnimation>
+                        <ScrollAnimation animateIn='bounceInRight' duration={1} animateOnce={true}>  
+                            <div className="module_content">
+                                <h1>Review List</h1>
+                                <p>Employer reviews the credit-based, organized list and picks the candidate(s) that they wish to move forward with based on their profile and the recruiters terms and conditions</p>
+                            </div> 
+                        </ScrollAnimation>
+                    </div>
+                    <div className="module_wrapper">
+                        <ScrollAnimation animateIn='bounceInLeft' duration={1} animateOnce={true}>  
+                            <div className="module_content">
+                                <h1>Engage</h1>
+                                <p>Employer can reach out to the recruiter(s) they’ve chosen to engage based on their candidate profile submission and terms and conditions via our messaging app, email, or phone</p>
+                            </div> 
+                        </ScrollAnimation>
+                        <ScrollAnimation animateIn='bounceInRight' duration={1} animateOnce={true}>  
+                                <div className="module_img module_img4"></div> 
+                        </ScrollAnimation>
+                    </div>
+                    <div className="module_wrapper greyBG">
+                        <div className="animated_half">
+                            <div className="module_content"> 
+                                <h1>Ready to find out more?</h1>
+                                <p>We are currently working towards finishing our plat- Sign Up form. Please sign up below to get word about when our FREE version launches. </p>
+                            </div>
+                        </div>
+                        <div className="animated_half"> 
+                            <NavLink to='/login'><div className="signUpBtnHome">Sign Up</div></NavLink> 
                         </div>
                     </div>
-                    <div className="homeBtnContainer"> 
-                        <NavLink to='/login'><div className="signUpBtnHome">Sign Up</div></NavLink> 
+                </div>
+                {/* end FOR DESKTOP - stacking properly */ }
+
+
+
+
+
+                {/* FOR MOBILE - stacking properly*/ }
+                <div className="showOnMobile">
+                    <div className="module_wrapper mc_4">
+                        <ScrollAnimation animateIn='bounceInLeft' duration={1} animateOnce={true}> 
+                            <div className="module_img module_img1"></div>
+                        </ScrollAnimation>
+                        <ScrollAnimation animateIn='bounceInRight' duration={1} animateOnce={true}>  
+                            <div className="module_content">
+                                <h1>Sign up</h1>
+                                <p>Employers sign up to post jobs exclusively to third-party recruiters. Recruiters sign up to gain access to jobs posted by employers seeking third-party recruitment services.</p>
+                            </div>
+                        </ScrollAnimation>
+                    </div>
+                    <div className="module_wrapper">
+                        <ScrollAnimation animateIn='bounceInLeft' duration={1} animateOnce={true}>
+                            <div className="module_img module_img2"></div>
+                        </ScrollAnimation>
+                        <ScrollAnimation animateIn='bounceInRight' duration={1} animateOnce={true}>
+                            <div className="module_content">
+                                <h1>Submit Profile</h1>
+                                <p>Recruiters submit their top candidate profiles along with terms and conditions to relevant job postings</p>
+                            </div> 
+                        </ScrollAnimation>
+                    </div>
+                    <div className="module_wrapper mc_4">
+                        <ScrollAnimation animateIn='bounceInLeft' duration={1} animateOnce={true}> 
+                            <div className="module_img module_img3"></div>
+                        </ScrollAnimation>
+                        <ScrollAnimation animateIn='bounceInRight' duration={1} animateOnce={true}>  
+                            <div className="module_content">
+                                <h1>Review List</h1>
+                                <p>Employer reviews the credit-based, organized list and picks the candidate(s) that they wish to move forward with based on their profile and the recruiters terms and conditions</p>
+                            </div> 
+                        </ScrollAnimation>
+                    </div>
+                    <div className="module_wrapper">
+                        <ScrollAnimation animateIn='bounceInLeft' duration={1} animateOnce={true}>  
+                            <div className="module_img module_img4"></div>  
+                        </ScrollAnimation>
+                        <ScrollAnimation animateIn='bounceInRight' duration={1} animateOnce={true}>
+                            <div className="module_content">
+                                <h1>Engage</h1>
+                                <p>Employer can reach out to the recruiter(s) they’ve chosen to engage based on their candidate profile submission and terms and conditions via our messaging app, email, or phone</p>
+                            </div> 
+                        </ScrollAnimation>
+                    </div>
+                    <div className="module_wrapper greyBG">
+                        <div className="animated_half">
+                            <div className="module_content"> 
+                                <h1>Ready to find out more?</h1>
+                                <p>We are currently working towards finishing our plat- Sign Up form. Please sign up below to get word about when our FREE version launches. </p>
+                            </div>
+                        </div>
+                        <div className="animated_half">
+                            <div className="module_content">
+                                <NavLink to='/login'><div className="signUpBtnHome">Sign Up</div></NavLink>
+                            </div>
+                        </div>
                     </div>
                 </div>
+                {/* FOR MOBILE - stacking properly*/ }
+
+
+
+ 
             </div>
         );
     }
