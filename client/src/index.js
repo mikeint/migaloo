62079b5ae7d51a17c069275e7958805ce59b1c5d
Michael Marcucci <onlycutch@gmail.com>
2019-04-04 22:25:27 -0400

    Update favicon

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./App"; 
 ReactDOM.render(
     <React.Fragment>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
         <App />
     </React.Fragment>, document.getElementById("root"));
