486bf5e27e69a220a8000d75740f62f6ead65442
Sansone <Michael.sansone@hotmail.com>
2019-04-15 19:05:03 -0400

    changing landing page styles

--- client/src/layout/LandingSection4/LandingSection4.js
+++ client/src/layout/LandingSection4/LandingSection4.js
@@ -49,32 +49,32 @@ class LandingSection4 extends Component {
                     </div>
                 </div>  */}
                      
-                <Parallax bgImage={section4Parallax} strength={300}> 
+                <Parallax bgImage={section4Parallax} strength={400}> 
                     <div className="sp_contain"> 
                         
                         <div className="sp_text">
                             <ScrollAnimation animateIn='fadeIn'>
-                                <div className="module_content"> 
+                                <div className="calloutSection"> 
                                     <h1>Ready to find out more?</h1>
                                     <p>We are near the release of our beta platform. Sign up now to be part of our first cohort of recruiters and&nbsp;employers</p>
                                 </div>
                             </ScrollAnimation>
+
+                            <div className="sp_choose">
+                                {!this.state.showSignUpButtons ? 
+                                    <div className="signUpBtnHome" onClick={this.showButtons}>Sign Up</div>
+                                :  
+                                <React.Fragment> 
+                                    <Link to='signUpFormEmployer'><div className="signUpBtnHome signUpBtnChoose"><img src={empImg} alt="" align="middle" />Employer</div></Link>
+                                    <Link to='signUpFormRecruiter'><div className="signUpBtnHome signUpBtnChoose"><img src={recImg} alt="" align="middle" />Recruiter</div></Link>
+                                </React.Fragment>
+                                }  
+                            </div>
                         </div>
                         
-                        <div className="sp_choose">
-                            {!this.state.showSignUpButtons ? 
-                                <div className="signUpBtnHome" onClick={this.showButtons}>Sign Up</div>
-                            :  
-                            <React.Fragment>
-                                <div className="chooseText">Please choose:</div>
-                                <Link to='signUpFormEmployer'><div className="signUpBtnHome signUpBtnChoose"><img src={empImg} alt="" align="middle" />Employer</div></Link>
-                                <Link to='signUpFormRecruiter'><div className="signUpBtnHome signUpBtnChoose"><img src={recImg} alt="" align="middle" />Recruiter</div></Link>
-                            </React.Fragment>
-                            }  
-                        </div>
                         
                     </div> 
-                    <div style={{ height: '500px' }}></div>
+                    <div style={{ height: '250px' }}></div>
                 </Parallax> 
             
  
