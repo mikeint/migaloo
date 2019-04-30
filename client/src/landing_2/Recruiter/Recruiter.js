f4ca366af1512d351b2bacd35a80d55b4db74246
Sansone <Michael.sansone@hotmail.com>
2019-04-29 21:30:51 -0400

    slowly, but surely

--- client/src/landing_2/Recruiter/Recruiter.js
+++ client/src/landing_2/Recruiter/Recruiter.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";  
 import './Recruiter.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import { Link } from 'react-router-dom';
 import NavBar from "../components/NavBar/NavBar"
  
 import whaleCool from "../../files/images/landingPage/whaleCool.png"; 
 import recImg from "../../files/images/recruiter_pick.png"; 
  
-class Recruiter extends Component { 
+class Recruiter extends Component {
+
+    componentDidMount(){
+        AOS.init({
+          duration : 500
+        })
+    }
+    
     render() {
         return ( 
             <div className="lp2_container">
@@ -17,19 +26,19 @@ class Recruiter extends Component {
                     <div className="fywText">find.your.whale</div>
 
                     <div className="fullRecruiterText">
-                        <div className="r1_Background r1_BackgroundLong">HI THERE ! I'M MIGALOO</div>
-                        <div className="r1_Background r1_BackgroundShorter">THE WHALE OF RECRUITMENT</div>
+                        <div className="r1_Background r1_BackgroundLong" data-aos="fade-right">HI THERE ! I'M MIGALOO</div>
+                        <div className="r1_Background r1_BackgroundShorter" data-aos="fade-right">THE WHALE OF RECRUITMENT</div>
                     </div>
                     
                     <div className="opacBackground">
                         <div className="flex">
-                            <div className="flex r_para">
+                            <div className="flex r_para" data-aos="fade-right">
                                 I'm here to help you save time & make more placements. 
                                 I believe every great recruiter deserves 
                                 a whale on their side who can who can add relevant, 
                                 retained jobs to their pipeline. 
                             </div>
-                            <div className="flex r_para">
+                            <div className="flex r_para" data-aos="fade-left">
                                 I will Deliver job reqs based on your 
                                 specialization from top companies who
                                 desperatley need you help but, who sadly, 
@@ -38,21 +47,21 @@ class Recruiter extends Component {
                             </div>
                         </div>
  
-                        <div className="flexCenter">
+                        <div className="flexCenter" data-aos="zoom-in">
                             <div className="whaleCool">
                                 <img src={whaleCool} alt="" />
                             </div> 
                             <div className="soundCool">
-                                <div className="sc_Background">SOUND COOL?</div>
+                                <div className="sc_Background" data-aos="zoom-in">SOUND COOL?</div>
                             </div>
                         </div>
 
-                        <div className="flex">
+                        <div className="flex" data-aos="zoom-in">
                             Im inviting you to make an even bigger splash in your market.<br/>
                             & join the whales of recruitment.
                         </div>
                         <div className="flex">
-                            <Link to='signUpFormRecruiter'><div className="signUpBtnHome signUpBtnChoose"><img src={recImg} alt="" align="middle" />Recruiter Sign Up</div></Link>
+                            <Link to='signUpFormRecruiter'><div className="signUpBtnHome"><img src={recImg} alt="" align="middle" />Recruiter Sign Up</div></Link>
                         </div>
                     </div>
 
