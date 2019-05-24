9dc89ba31d008d4717f130c15f59c661df22a5bd
michael <michael.sansone@hotmail.com>
2019-05-24 00:35:21 -0400

    started moving nav bar

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -11,7 +11,7 @@ import Chat from '@material-ui/icons/Chat';
 import AccountBalance from '@material-ui/icons/AccountBalance';
 import Notifications from '../Notifications/Notifications';
 import { withStyles } from '@material-ui/core/styles';
-import Tabs from '@material-ui/core/Tabs';
+//import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 
 function LinkTab(props) {
@@ -22,25 +22,27 @@ function getByPath(object, path) {
 };
 const styles = theme => ({
     tabsContainer:{
-        width: "100%"
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%', 
     },
-    linkButton:{
-        maxWidth: "200px"
-    },
+    linkButton:{ 
+        display: 'flex',  
+    },  
     tabsIndicator: {
-      backgroundColor: '#6f90a1'
+      backgroundColor: '#6f90a14d',
+      height: "80px"
     },
     leftButton:{
-        width: "50px",
-        minWidth: "50px",
-        maxWidth: "50px",
-        padding: 0,
-        marginLeft: "auto"
+        width: '50px',
+        minWidth: '50px',
+        maxWidth: '50px',
+        padding: 0, 
     },
     smallButton:{
-        width: "50px",
-        minWidth: "50px",
-        maxWidth: "50px",
+        width: '50px',
+        minWidth: '50px',
+        maxWidth: '50px',
         padding: 0
     }
 })
@@ -48,30 +50,30 @@ const navMappings = {
     1:{'/recruiter':[ // Recruiter
             {
                 icon:<Search/>,
-                link:"/recruiter/jobList",
-                name:"Job Search",
+                link:'/recruiter/jobList',
+                name:'Job Search',
                 className: 'linkButton'
             },
             {
                 icon:<ListAlt/>,
-                link:"/recruiter/candidateList",
-                name:"Candidate List",
+                link:'/recruiter/candidateList',
+                name:'Candidate List',
                 className: 'linkButton'
             },
             {
                 icon:<Chat/>,
-                link:"/recruiter/chat",
-                name:"Chat",
+                link:'/recruiter/chat',
+                name:'Chat',
                 className: 'linkButton'
             },
             {
                 icon:<AccountCircle />,
-                link:"/recruiter/profile",
+                link:'/recruiter/profile',
                 className: 'leftButton'
             },
             {
                 icon:<AccountBalance />,
-                link:"/recruiter/account",
+                link:'/recruiter/account',
                 className: 'smallButton',
                 showOnState: ['user', 'isPrimary']
             }
@@ -81,30 +83,30 @@ const navMappings = {
         '/employer':[ // Employer
             {
                 icon:<ListAlt/>,
-                link:"/employer/activeJobs",
-                name:"Active Jobs",
+                link:'/employer/activeJobs',
+                name:'Active Jobs',
                 className: 'linkButton'
             },
             {
                 icon:<Search/>,
-                link:"/employer/postAJob",
-                name:"Post a Job",
+                link:'/employer/postAJob',
+                name:'Post a Job',
                 className: 'linkButton'
             },
             {
                 icon:<Chat/>,
-                link:"/employer/chat",
-                name:"Chat",
+                link:'/employer/chat',
+                name:'Chat',
                 className: 'linkButton'
             },
             {
                 icon:<AccountBalance />,
-                link:"/employer/accounts",
+                link:'/employer/accounts',
                 className: 'leftButton'
             },
             {
                 icon:<AccountCircle />,
-                link:"/employer/profile",
+                link:'/employer/profile',
                 className: 'smallButton'
             }
         ]
@@ -161,9 +163,10 @@ class NavBar extends React.Component{
         const { classes } = this.props;
         return (
             <React.Fragment> 
-                <AppBar position="static">
+                <AppBar position='static'>
+                    <Notifications/>
                     <Toolbar>
-                        <Tabs variant="fullWidth"
+                        <div variant='fullWidth'
                             value={this.state.page}
                             className={classes.tabsContainer}
                             classes={{indicator:classes.tabsIndicator}}
@@ -173,8 +176,7 @@ class NavBar extends React.Component{
                                     return <LinkTab className={classes[d.className]} label={d.name} icon={d.icon} key={i} to={d.link} />
                                 })
                             }
-                            <Notifications/>
-                        </Tabs>
+                        </div>
                     </Toolbar>
                 </AppBar>
             </React.Fragment>
