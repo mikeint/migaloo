5437d7e646d39dc7e6982f185a3b4fd3e38e2927
michael <michael.sansone@hotmail.com>
2019-07-09 19:17:42 -0400

    changed icons in candidate info

--- client/src/pages/recruiter/CandidateList/ExpandableRow/ExpandableRow.js
+++ client/src/pages/recruiter/CandidateList/ExpandableRow/ExpandableRow.js
@@ -10,25 +10,34 @@ import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import Button from '@material-ui/core/Button';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import Drawer from '@material-ui/core/Drawer';
+import Drawer from '@material-ui/core/Drawer'; 
 
+ 
+import ResumeImage from '@material-ui/icons/CloudUpload'; 
+import GetApp from '@material-ui/icons/GetApp'; 
+import EmailImage from '@material-ui/icons/MailOutline'; 
+import TagsImage from '@material-ui/icons/Label'; 
+import ExperienceImage from '@material-ui/icons/Computer'; 
+import PostImage from '@material-ui/icons/OpenInNew';
 import whaleImage from '../../../../files/images/landingPage/tail.png' 
-import resumeImage from '../../../../files/images/candidateIcons/resumeIcn.png'
-
-import emailImage from '../../../../files/images/candidateIcons/email_icon.png'
-import tagsImage from '../../../../files/images/candidateIcons/tags.png' 
-import experienceImage from '../../../../files/images/candidateIcons/experienceImage.png' 
-
-import postImage from '../../../../files/images/candidateIcons/postImage.png' 
+  
 
 
 const styles = theme => ({
     root: {
-        boxShadow: 'none !important'
+        boxShadow: 'none !important',
+    },
+    dropDown: {
+        background: '#263c540d',
+        boxShadow:' inset 2px 0px 2px 0px #546f82',
+
+        '@media (max-width: 1024px)': { 
+            padding: '8px 8px 8px',
+        },
     },
     drawer:{ 
         minWidth: "300px",
-        position: "relative"
+        position: "relative",
     },
     button: {
         margin:'10px',
@@ -105,7 +114,7 @@ class ExpandableRow extends React.Component{
                     </ExpansionPanelSummary>
 
                     {/* DROPDOWN CONTAINER */}
-                    <ExpansionPanelDetails>
+                    <ExpansionPanelDetails className={classes.dropDown}>
                         <div className="dropDownContainer">
                             <div className="a_container">
                                 <div className="acceptedContainer">
@@ -122,17 +131,17 @@ class ExpandableRow extends React.Component{
 
                             <div className="infoRowContainer">
                                 <div className="infoRow">
-                                    <div className="icon"><img src={tagsImage} alt="tagsImage" /></div>
+                                    <div className="icon"><TagsImage/></div>
                                     <div className="title"><b>Tags: </b></div>
                                     <div className="item">{candidateData.tagNames.join(", ")}</div>
                                 </div>
                                 <div className="infoRow">
-                                    <div className="icon"><img src={experienceImage} alt="experienceImage" /></div>
+                                    <div className="icon"><ExperienceImage/></div>
                                     <div className="title"><b>Experience: </b></div>
                                     <div className="item">{candidateData.experience}</div>
                                 </div>
                                 <div className="infoRow">
-                                    <div className="icon"><img src={emailImage} alt="emailImage" /></div>
+                                    <div className="icon"><EmailImage/></div>
                                     <div className="title"><b>Email: </b></div>
                                     <div className="item">{candidateData.email}</div>
                                 </div>
@@ -150,7 +159,7 @@ class ExpandableRow extends React.Component{
 
                                 <Button className={classes.button} variant="contained" color="primary" onClick={this.showUpload}>
                                     <div className="buttonsContainer">
-                                        <div className="image"><img className="img2" src={resumeImage} alt="resumeImage" /></div> 
+                                        <div className="image"><ResumeImage/></div> 
                                         <div className="text">Upload Resume</div>
                                     </div> 
                                 </Button>  
@@ -158,7 +167,7 @@ class ExpandableRow extends React.Component{
                                 {candidateData.resumeId != null &&
                                     <Button className={classes.button} variant="contained" color="primary" onClick={this.getResumeURL}>
                                         <div className="buttonsContainer">
-                                            <div className="image"><img className="img2" src={resumeImage} alt="resumeImage" /></div> 
+                                            <div className="image"><GetApp/></div> 
                                             <div className="text">View Resume</div>
                                         </div>
                                     </Button>
@@ -167,12 +176,11 @@ class ExpandableRow extends React.Component{
                                 {postData &&
                                     <Button className={classes.button} variant="contained" color="primary" onClick={this.postToJob}> 
                                         <div className="buttonsContainer">
-                                            <div className="image"><img className="img3" src={postImage} alt="postImage" /></div> 
+                                            <div className="image"><PostImage/></div> 
                                             <div className="text">Post to Job</div>
                                         </div>
                                     </Button>
-                                }
-
+                                } 
                             </div> 
  
                             <Drawer
@@ -187,93 +195,8 @@ class ExpandableRow extends React.Component{
                                                             handleClose={()=>this.setState({showPostJob:false})} />
                             </Drawer>
                             {this.state.showUpload && <UploadResume id={candidateData.candidateId} handleClose={this.handleClose} />}
-                       
-
-
-                        </div>
-
-
-
-
-
-
-
-
-{/*                         <div className="flex">
-                            <div className="flexColumn">
-                                <div className="flex-item">
-                                    <div>
-                                        <div className="created-info-item"> 
-                                            <span className="heading">Created: </span> 
-                                            <span className="headingInfo">{candidateData.created}</span>
-                                        </div>
-                                    </div>
-                                    <div className="info-container"> 
-                                        <div>
-                                            <span className="email_icon"></span>
-                                            <div className="candidate-info-item">
-                                                <span className="heading">Email: </span> 
-                                                <span className="headingInfo">{candidateData.email}</span>
-                                            </div>
-                                        </div> 
-                                        <div>
-                                            <span className="experience_icon"></span>
-                                            <div className="candidate-info-item">
-                                                <span className="heading">Experience: </span> 
-                                                <span className="headingInfo">{candidateData.experience}</span>
-                                            </div>
-                                        </div>
-                                        <div>
-                                            <span className="tags_icon"></span>
-                                            <div className="candidate-info-item">
-                                                <span className="headingInfo">
-                                                    {candidateData.tagNames && <span className="rowMargin">
-                                                    <span className="heading">Tags: </span> {candidateData.tagNames.join(", ")}</span>}
-                                                </span>
-                                            </div>
-                                        </div>
-                                        <div>
-                                            <span className="posted_icon"></span>
-                                            <div className="candidate-info-item">
-                                                <span className="heading">Posted to Job: </span> 
-                                                <span className="headingInfo">{candidateData.postedCount} time(s)</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
- 
-                                <Button variant="contained" color="primary" onClick={this.searchJobsForCandidates}>Search Jobs</Button>
-                            </div>
-                            <div className="flexColumn">
-                                <div className="flex-item"> 
-                                    <img className="candidate_image" src="http://placehold.it/100x100" alt="" />
-                                    <p><span className="accepted_icon"></span><span className="heading">Accepted by Postings: </span> {candidateData.acceptedCount} time(s)</p>
-                                    <p><span className="notAccepted_icon"></span><span className="heading">Not Accepted by Postings: </span> {candidateData.notAcceptedCount} time(s)</p> 
-
-                                    <div className="resumeButtons">
-                                        {candidateData.resumeId != null && <Button variant="contained" color="primary" onClick={this.getResumeURL}>View Resume</Button>}
-                                        <Button variant="contained" color="primary" onClick={this.showUpload}>Upload Resume</Button>
-                                    </div>
-                                    {postData && <div className="resumeButtons">
-                                        <Button variant="contained" color="primary" onClick={this.postToJob}>Post Candidate to Job</Button>
-                                    </div>}
-                                    
-                                    <Drawer
-                                        anchor="bottom"
-                                        className={classes.drawer}
-                                        open={this.state.showPostJob}
-                                        onClose={()=>this.setState({"showPostJob":false})}
-                                        // onOpen={()=>this.setState({"showPostJob":true})}
-                                    > 
-                                        <PostCandidateToJob candidate={candidateData}
-                                                                    job={postData}
-                                                                    handleClose={()=>this.setState({showPostJob:false})} />
-                                    </Drawer>
-                                    
-                                    {this.state.showUpload && <UploadResume id={candidateData.candidateId} handleClose={this.handleClose} />}
-                                </div> 
-                            </div>
-                        </div> */}
+                        
+                        </div> 
                     </ExpansionPanelDetails>
                 </ExpansionPanel>
             </div> 
